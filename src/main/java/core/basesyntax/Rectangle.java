2de6fc9d2888a07b4175082dd1ba76d9cd660e04
Tykhon M <tykhon.milinevskyi@edvantis.com>
2026-01-04 23:51:31 +0200

    Remove Object methods

--- src/main/java/core/basesyntax/Rectangle.java
+++ src/main/java/core/basesyntax/Rectangle.java
@@ -1,8 +1,9 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class Rectangle {
+
+    private static final int HASH_MULTIPLIER = 31;
+
     private Integer width;
     private Integer length;
     private String color;
@@ -29,12 +30,19 @@ public class Rectangle {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Rectangle otherRectangle  = (Rectangle) o;
-        return Objects.equals(width, otherRectangle.width) && Objects.equals(length, otherRectangle.length) && Objects.equals(color, otherRectangle.color);
+        Rectangle otherRectangle = (Rectangle) o;
+        boolean sameWidth = width == null ? otherRectangle.width == null : width.equals(otherRectangle.width); // if null -> return true only for null, else just compare
+        boolean sameLength = length == null ? otherRectangle.length == null : length.equals(otherRectangle.length);
+        boolean sameColor = color == null ? otherRectangle.color == null : color.equals(otherRectangle.color);
+        return sameWidth && sameLength && sameColor;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(width, length, color);
+        int result = 1;
+        result = HASH_MULTIPLIER * result + (width == null ? 0 : width.hashCode());
+        result = HASH_MULTIPLIER * result + (length == null ? 0 : length.hashCode());
+        result = HASH_MULTIPLIER * result + (color == null ? 0 : color.hashCode());
+        return result;
     }
 }
